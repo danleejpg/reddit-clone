4856eaed91a16895bad669b0bada5eb597f622f3
danleejpg <108959932+danleejpg@users.noreply.github.com>
2022-08-11 05:59:01 -0400

    added postbox func and feed retrieval

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 import Header from '../components/Header';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     // in the next auth library
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
+        <Toaster />
         <div className='h-screen overflow-y-scroll bg-slate-200'>
           {/* Having the header here means that
             the header exists on every page */}
